e2d0b9f06ecc8f1022ebb66fc0a9ca2f4cd1ffc3
shaik80 <sh.mudassir98@gmail.com>
2019-10-24 00:15:44 -0700

    testing files working condition

---
+++ controllers/introductioncontrollers.js
@@ -0,0 +1,11 @@
+const express = require('express');
+let router = express();
+const mongoose = require('mongoose');
+
+
+router.get('/', (res,req) =>{
+    req.render('./employee/index')
+})
+
+
+module.exports = router; 
