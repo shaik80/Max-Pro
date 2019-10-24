f416d086fa651774b87c09dc9c999252613f04de
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-10-24 16:38:03 +0530

    register page

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ const router =express.Router();
 
 
 //Login page
-router.get('/login',(req,res)=>res.send('Login'))
+router.get('/login',(req,res)=>res.render('login'))
 
 //Register page
-router.get('/register',(req,res)=>res.send('Register'))
+router.get('/register',(req,res)=>res.render('register'))
 module.exports=router;
