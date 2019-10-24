af449494dda028ad56489145defd186ef696e7db
shaik80 <sh.mudassir98@gmail.com>
2019-10-23 23:43:14 -0700

    database skeleton

---
+++ config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+//connect db 
+mongoose.connect(
+    '',{ useNewUrlParser: true, useUnifiedTopology: true},  (err) => {
+    if (!err) { console.log("db connection sucessed")}
+    else { console.log("db connection error " + err)}
+})
+
+require('../models/')
