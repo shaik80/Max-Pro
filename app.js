458d3457565cad17969b09783ab7cc2dfafbe6aa
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-10-24 12:52:32 +0530

    importing users route into app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app= express();
 
 //routes
 app.use('/',require('./routes/index'));
-
+app.use('/users',require('./routes/users'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,console.log(`server started on port ${PORT}`));
