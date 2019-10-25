742f5050857e9390a023084410d5d0dcbc05860d
shaik80 <sh.mudassir98@gmail.com>
2019-10-24 22:50:45 -0700

    mongo db connection link in added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //connect db 
 mongoose.connect(
-    '',{ useNewUrlParser: true, useUnifiedTopology: true},  (err) => {
+    'mongodb+srv://root:root@cluster0-mp92k.mongodb.net/maxpro?retryWrites=true&w=majority',{ useNewUrlParser: true, useUnifiedTopology: true},  (err) => {
     if (!err) { console.log("db connection sucessed")}
     else { console.log("db connection error " + err)}
 })
