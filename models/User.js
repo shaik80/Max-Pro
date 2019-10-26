34aa6d94d67e0db072bb25d01cee81a25bae9b57
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-10-26 13:46:58 +0530

    updated schema

--- models/User.js
+++ models/User.js
@@ -18,9 +18,7 @@ const UserSchema=new mongoose.Schema({
     date:{
         type:Date,
         default:Date.now
-    }},
-    {
-        collection: 'User'
-      });
+    }
+});
 const User=mongoose.model('User',UserSchema);
 module.exports=User;
