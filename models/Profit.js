b7c07c03b29c26ff799e74543e565a1144014354
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-10-28 23:56:06 +0530

    Mongoose schema

---
+++ models/Profit.js
@@ -0,0 +1,23 @@
+const mongoose =require('mongoose');
+
+
+const ProfitSchema=new mongoose.Schema({
+
+    itemname:{
+        type:[],
+        required:true
+    },
+    profit:{
+        type:[],
+        required:true
+    },
+    budget:{
+        type:[],
+        required:true
+    },
+    demand:{
+        type:[],
+        required:true
+    }
+});
+const Profit=mongoose.model('Profit',ProfitSchema);
