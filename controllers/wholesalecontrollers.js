f5e1f034b383e4f6fb4e843aad061ad3c9f4c91e
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-12 05:19:25 -0800

    Update wholesalecontrollers.js

--- controllers/wholesalecontrollers.js
+++ controllers/wholesalecontrollers.js
@@ -7,47 +7,43 @@ const Wholesale = mongoose.model('Wholesale')
 
 
 
-router.get('/wholesale', (req,res) =>{
-    res.render('./wholesale/addoredit',{
+router.get('/wholesale', (req, res) => {
+    res.render('./wholesale/addoredit', {
         wholesale: req.body
     })
 })
 
-router.post('/wholesale',(req,res) =>{
-    InsertRecord(req,res)
+router.post('/wholesale', (req, res) => {
+    InsertRecord(req, res)
 })
 
-function InsertRecord(req,res){
-    let wholesale =new Wholesale()
+function InsertRecord(req, res) {
+    let wholesale = new Wholesale()
     wholesale.ProductName = req.body.ProductName;
     wholesale.ShopName = req.body.ShopName;
     wholesale.fixedQty = req.body.fixedQty;
     wholesale.totalpackage = req.body.totalpackage;
     wholesale.CostPrice = req.body.CostPrice;
     wholesale.SellingPrice = req.body.SellingPrice;
-    wholesale.save((err,doc) => {
-        if(!err){
+    wholesale.save((err, doc) => {
+        if (!err) {
             res.redirect('/wholesale');
-        }
-        else{
-            if(err.name == 'ValidationError'){
-                handleerror(err,req.body)     
-            }
-            else
-            {
-                console.log("error during insertion"+err)
+        } else {
+            if (err.name == 'ValidationError') {
+                handleerror(err, req.body)
+            } else {
+                console.log("error during insertion" + err)
             }
         }
     })
 }
 
 
-router.get('/wholesales', async (req,res)=>{
-    try{
+router.get('/wholesales', async (req, res) => {
+    try {
         const wholesale = await Wholesale.find();
         res.send(wholesale)
-    }
-    catch(err){
+    } catch (err) {
         res.json(err)
     }
 });
@@ -57,4 +53,4 @@ router.get('/wholesales', async (req,res)=>{
 
 
 
-module.exports = router; 
+module.exports = router;
