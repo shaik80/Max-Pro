6b0f248ea6e00c3e60eeb149ee34b8ab25cbce9d
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-12 00:37:00 -0800

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,45 +1,41 @@
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-
-
-
-// Load User model
 const User = mongoose.model('User');
+
 module.exports = function (passport) {
-  passport.use(
-    new LocalStrategy({
-      usernameField: 'email'
-    }, (email, password, done) => {
-      // Match user
-      User.findOne({
-        email: email
-      }).then(user => {
+  passport.use(new LocalStrategy({
+    usernameField: 'emailid'
+  }, (emailid, password, done) => {
+    console.log(emailid, password)
+    User.findOne({
+      emailid: emailid
+      })
+      .then(user => {
         if (!user) {
           return done(null, false, {
-            message: 'That email is not registered'
-          });
+            message: "No User Found"
+          })
         }
-        // Match password
         bcrypt.compare(password, user.password, (err, isMatch) => {
           if (err) throw err;
           if (isMatch) {
             return done(null, user);
           } else {
             return done(null, false, {
-              message: 'Password incorrect'
-            });
+              message: "Password Incorrect"
+            })
           }
-        });
-      });
-    })
-  );
+        })
+      })
+  }));
   passport.serializeUser(function (user, done) {
     done(null, user.id);
   });
+
   passport.deserializeUser(function (id, done) {
     User.findById(id, function (err, user) {
       done(err, user);
     });
   });
-};
+}
