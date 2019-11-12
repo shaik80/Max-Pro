eb53688e98ba230afbfcfc50a34a30ee1bbfa4d1
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-12 05:19:33 -0800

    Update auth.js

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,15 +1,15 @@
 module.exports = {
-    ensureAuthenticated: function(req, res, next) {
-      if (req.isAuthenticated()) {
-        return next();
-      }
-      req.flash('error_msg', 'Please log in to view that resource');
-      res.redirect('/404');
-    },
-    forwardAuthenticated: function(req, res, next) {
-      if (!req.isAuthenticated()) {
-        return next();
-      }
-      res.redirect('/js');
+  ensureAuthenticated: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
     }
-  };
+    req.flash('error_msg', 'Please log in to view that resource');
+    res.redirect('/404');
+  },
+  forwardAuthenticated: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/js');
+  }
+};
