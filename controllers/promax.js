fdbd02fb1ea7a9bd677ee2b1855ad08f72051211
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-12 02:18:03 -0800

    backup for feature implimentation

--- controllers/promax.js
+++ controllers/promax.js
@@ -9,64 +9,64 @@ let itemSelection = [];
 let quantitySelection = [];
 let answer = [];
 
-router.get('/', async (res, req) => {
-    const demands = await Demand.find().sort({
-        "demand": -1
-    });
-    let sortdemandprofit = []
-    const demanddata = demands.map((v, k) => {
-        return {
-            demandproductname: v.ProductName,
-            demandvalue: v.demand
-        }
-    })
-    const wholesales = await Wholesale.find();
-    wholesales.map((v, k) => {
-            return {
-                id: v._id,
-                productname: v.ProductName,
-                Shopname: v.ShopName,
-                onepackageprofit: (v.SellingPrice - v.CostPrice),
-                CostPrice: v.CostPrice,
-                FixedQty: v.fixedQty,
-                Totalpackage: v.totalpackage,
-            }
-        })
-        .sort((a, b) => b.onepackageprofit - a.onepackageprofit)
-        .forEach((wholesalesvalue, wholesaleskeys) => {
-            demanddata.forEach((demandsvalue, demandskeys) => {
-                if (wholesalesvalue.productname === demandsvalue.demandproductname) {
-                    if (sortdemandprofit[demandskeys] === undefined || sortdemandprofit[demandskeys].Productname !== wholesalesvalue.productname && sortdemandprofit[demandskeys].profit > wholesalesvalue.productname) {
-                        let a = JSON.parse(
-                            '{"Productname":' + "\"" + wholesalesvalue.productname + "\"" +
-                            ',"id":' + "\"" + wholesalesvalue.id + "\"" +
-                            ',"Onepackageprofit":' + wholesalesvalue.onepackageprofit +
-                            ',"Shop":' + "\"" + wholesalesvalue.Shopname + "\"" +
-                            ',"Costprice":' + wholesalesvalue.CostPrice +
-                            ',"FixedQty":' + wholesalesvalue.FixedQty +
-                            ',"Totalpackages":' + wholesalesvalue.Totalpackage +
-                            ',"Demand":' + demandsvalue.demandvalue +
-                            '}')
-                        sortdemandprofit.push(a) // console.log([...new Set(a)])
-                    } else {
-                        console.log("error")
-                    }
-                }
-            })
-        })
-    // console.log(sortdemandprofit)
-    // console.log(sortdemandprofit)
-    // sortdemandprofit.forEach((v,k) =>{
-    //     console.log(v);
-    // })
+// router.get('/', async (res, req) => {
+//     const demands = await Demand.find().sort({
+//         "demand": -1
+//     });
+//     let sortdemandprofit = []
+//     const demanddata = demands.map((v, k) => {
+//         return {
+//             demandproductname: v.ProductName,
+//             demandvalue: v.demand
+//         }
+//     })
+//     const wholesales = await Wholesale.find();
+//     wholesales.map((v, k) => {
+//             return {
+//                 id: v._id,
+//                 productname: v.ProductName,
+//                 Shopname: v.ShopName,
+//                 onepackageprofit: (v.SellingPrice - v.CostPrice),
+//                 CostPrice: v.CostPrice,
+//                 FixedQty: v.fixedQty,
+//                 Totalpackage: v.totalpackage,
+//             }
+//         })
+//         .sort((a, b) => b.onepackageprofit - a.onepackageprofit)
+//         .forEach((wholesalesvalue, wholesaleskeys) => {
+//             demanddata.forEach((demandsvalue, demandskeys) => {
+//                 if (wholesalesvalue.productname === demandsvalue.demandproductname) {
+//                     if (sortdemandprofit[demandskeys] === undefined || sortdemandprofit[demandskeys].Productname !== wholesalesvalue.productname && sortdemandprofit[demandskeys].profit > wholesalesvalue.productname) {
+//                         let a = JSON.parse(
+//                             '{"Productname":' + "\"" + wholesalesvalue.productname + "\"" +
+//                             ',"id":' + "\"" + wholesalesvalue.id + "\"" +
+//                             ',"Onepackageprofit":' + wholesalesvalue.onepackageprofit +
+//                             ',"Shop":' + "\"" + wholesalesvalue.Shopname + "\"" +
+//                             ',"Costprice":' + wholesalesvalue.CostPrice +
+//                             ',"FixedQty":' + wholesalesvalue.FixedQty +
+//                             ',"Totalpackages":' + wholesalesvalue.Totalpackage +
+//                             ',"Demand":' + demandsvalue.demandvalue +
+//                             '}')
+//                         sortdemandprofit.push(a) // console.log([...new Set(a)])
+//                     } else {
+//                         console.log("error")
+//                     }
+//                 }
+//             })
+//         })
+//     // console.log(sortdemandprofit)
+//     // console.log(sortdemandprofit)
+//     // sortdemandprofit.forEach((v,k) =>{
+//     //     console.log(v);
+//     // })
 
 
 
-    req.render("./employee/maxprofit", {
+//     req.render("./employee/maxprofit", {
 
-    })
+//     })
 
-})
+// })
 
 router.post('/', (req, res) => {
 
