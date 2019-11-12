9c23e0f0c6428a610b6b4d9e781f48c9a759f8f8
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-12 05:19:08 -0800

    Update indexcontrollers.js

--- controllers/indexcontrollers.js
+++ controllers/indexcontrollers.js
@@ -4,6 +4,12 @@ const router =express.Router();
 router.get('/',(req,res)=>res.render('welcome'))
 
 //Dashboard
-router.get('/dashboard',(req,res)=>res.render('./employee/dashboard'));
+router.get('/dashboard1',(req,res)=>{
+    res.render('./employee/dashboard')
+});
+router.get('/login', (req, res) => res.render('./general/login'));
+router.get('/register', (req, res) => res.render('./general/register'));
+router.get('/dashboard', (req, res) => res.render('dashboard'));
+
 
 module.exports=router;
