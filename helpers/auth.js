7539e30e9ccddc931229176de7334e43586d29b4
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-12 00:37:12 -0800

    Update auth.js

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,15 +1,9 @@
 module.exports = {
-  ensureAuthenticated: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    req.flash('error_msg', 'Please log in to view that resource');
-    res.redirect('/login');
-  },
-  forwardAuthenticated: function (req, res, next) {
-    if (!req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect('/js');
+  ensureAuthenticated: function (req,res,next){
+      if(req.isAuthenticated()){
+          return next();
+      }
+      req.flash('error_msg' , 'Not Authenticated')
+      res.redirect('/login')
   }
-};
+}
