165075f3761324fef00ffd11ebe306e7be8bb115
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-12 05:19:42 -0800

    Update Profit.models.js

--- models/Profit.models.js
+++ models/Profit.models.js
@@ -1,27 +1,27 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 
 
-const ProfitSchema=new mongoose.Schema({
+const ProfitSchema = new mongoose.Schema({
 
-    items:{
-        type:[String],
-        required:true
+    items: {
+        type: [String],
+        required: true
     },
-    profit:{
-        type:[Number],
-        required:true
+    profit: {
+        type: [Number],
+        required: true
     },
-    mybudget:{
-        type:Number,
-        required:true
+    mybudget: {
+        type: Number,
+        required: true
     },
-    mydemand:{
-        type:[Number],
-        required:true
+    mydemand: {
+        type: [Number],
+        required: true
     },
-    price:{
-        type:[Number],
-        required:true
+    price: {
+        type: [Number],
+        required: true
     }
 });
-mongoose.model('Profit',ProfitSchema);
+mongoose.model('Profit', ProfitSchema);
