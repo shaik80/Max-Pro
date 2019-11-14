7cdd57b6bddbea988c90d08d7f134c19ab7d7e89
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-14 00:44:45 -0800

    change port into capital PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('./config/db');
 const express = require('express');
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const path = require('path');
 const cors = require('cors');
 const exphbs = require('express-handlebars');
