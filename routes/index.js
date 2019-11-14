58ee68858306eecbf19531da0535a6911beff61c
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-11-13 21:20:36 -0800

    remove all

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router =express.Router();
-// welcome page
-router.get('/',(req,res)=>res.render('welcome'))
-
-//Dashboard
-router.get('/dashboard',(req,res)=>res.render('dashboard'));
-
-module.exports=router;
